63f8c5963ba1c95ab03d561356907700b876d77b
conceptworksx <conceptworksx@gmail.com>
2026-04-27 12:20:29 +0530

    Fix method signature in NewsAnalyst class

--- agents/analysis/news_analyst.py
+++ agents/analysis/news_analyst.py
@@ -6,7 +6,7 @@ class NewsAnalyst(BaseAgent):
     prompt_path = "prompts/news_analyst_prompt.yaml"
     tools       = [get_company_news]
 
-    def run(self , ticker : str) -> str:
+    def run(self , ticker : str):
 
         messages = [HumanMessage(content=f"Analysize the news for : {ticker}")]
         return self._invoke(messages)
